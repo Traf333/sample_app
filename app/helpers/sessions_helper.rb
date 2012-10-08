b33a78c515b610e67b370852f04505af3d958416
Igor Trofimov <igortr333@gmail.com>
2012-10-08 17:13:40 +0300

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,10 @@ module SessionsHelper
     @current_user ||= user_from_remember_token
   end
 
+  def current_user?(user)
+    user == current_user    
+  end
+
   def signed_in?
     !current_user.nil?
   end
@@ -22,6 +26,15 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default) 
+    session.delete(:return_to)   
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath    
+  end
+
   private
 
     def user_from_remember_token
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Igor Trofimov"
-    email    "igortr333@gmail.ru"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" } 
     password "TitorenkO"
     password_confirmation "TitorenkO"
+
+    factory :admin do
+      admin true
+    end
   end
 end
